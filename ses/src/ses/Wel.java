7a387bcb2189915c59d9790f3540fd8bf1c742ec
welcome <welcome@suman>
2018-05-10 00:03:15 +0530

    night stuff

---
+++ ses/src/ses/Wel.java
@@ -0,0 +1,18 @@
+package ses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Wel {
+	
+     @Test
+	public  void testloginscreen() {
+		// TODO Auto-generated method stub
+    	 
+    	 
+
+		  System.out.println("login screen ");
+	}
+
+}
