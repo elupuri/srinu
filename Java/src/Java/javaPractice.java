9606a52bb8e226d28001e320a0baf1d11fb9a65c
welcome <welcome@suman>
2018-05-14 18:19:29 +0530

    java second

---
+++ Java/src/Java/javaPractice.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class javaPractice {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to suman");
+		System.out.println("welcome to elupuri");
+
+	}
+
+}
